8eb7e49716ac59fc757ee10c4af16a80a827ae8c
ArranisAwesome <70060472+ArranisAwesome@users.noreply.github.com>
2021-11-04 15:45:16 +0000

    join channel

--- index.js
+++ index.js
@@ -57,6 +57,25 @@ for (const folder of commandFolders) {
     }
 }
 
+client.on("guildMemberAvailable", function (member) {
+    const newchan = await member.guild.channels.create(`testing-${member.user.username}`, { // Create Channel
+        type: 'text',
+        topic: `${member.username} Calculator Testing Channel`,
+        parent: "903031870420308018",
+        permissionOverwrites: [
+            {
+                id: member.id,
+                allow: ["SEND_MESSAGES", "VIEW_CHANNEL"]
+            },
+            {
+                id: member.guild.roles.everyone,
+                deny: ["VIEW_CHANNEL"]
+            },
+        ],
+    }); // Create Channel
+    await newchan.send({ content: "<@" + member.id + ">\nUse `/calculator`" })
+    return
+});
 
 process.on("unhandledRejection", async (error) => {
     console.log(error)
